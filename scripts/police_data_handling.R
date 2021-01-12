e02a8514a8049888ef4bd18798cdf9edcd03ebc4
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2021-01-12 16:31:15 +0000

    data handling and gini

--- scripts/police_data_handling.R
+++ scripts/police_data_handling.R
@@ -1,10 +1,15 @@
 # Load packages.
+library(lorenzgini)
+library(spdep)
 library(readr)
 library(dplyr)
 library(tidyr)
 library(purrr)
 library(stringr)
+library(ggplot2)
 library(sf)
+
+
 
 # Archive data downloaded January 2012, covering the 3-year period up to and including November 2020.
 # download.file(url = "https://data.police.uk/data/archive/2020-11.zip", destfile = "data/archive2020-11.zip")
@@ -20,7 +25,7 @@ list_2019 <- paste("data/", list.files("data", pattern = glob2rx("2019*street.cs
 data_2020 <- lapply(list_2020, read_csv)
 data_2019 <- lapply(list_2019, read_csv)
 
-# Bind each in to data frames. For now, we keep them separate.
+# Bind each in to data frames. For now, we keep the years separate.
 df_2020 <- bind_rows(data_2020)
 df_2019 <- bind_rows(data_2019)
 
@@ -63,18 +68,18 @@ df_2019_nm_sub <- drop_na(df_2019_sub, `LSOA code`)
 # Remove previous objects to free up memory if needed.
 rm(df_2020_sub, df_2019_sub)
 
-# Aggregate by month (N = 11), crime type (N = 14), and LSOA (N = ~33,000). 
+# Aggregate by month (N = 11), crime type (N = 14), and LSOA (N = ~33,000). Keep 'Year' in for later use.
 df_2020_agg <- df_2020_nm_sub %>% 
-  group_by(`Crime type`, Month, `LSOA code`) %>% 
+  group_by(`Crime type`, Month, `LSOA code`, Year) %>% 
   summarise(crime_count = n()) %>% 
   ungroup() %>% 
-  complete(`Crime type`, Month, `LSOA code`, fill = list(crime_count = 0))
+  complete(`Crime type`, Month, `LSOA code`, Year, fill = list(crime_count = 0))
 
 df_2019_agg <- df_2019_nm_sub %>% 
-  group_by(`Crime type`, Month, `LSOA code`) %>% 
+  group_by(`Crime type`, Month, `LSOA code`, Year) %>% 
   summarise(crime_count = n()) %>% 
   ungroup() %>% 
-  complete(`Crime type`, Month, `LSOA code`, fill = list(crime_count = 0))
+  complete(`Crime type`, Month, `LSOA code`, Year, fill = list(crime_count = 0))
 
 # Check number of each LSOAs appearing in each year. There is a difference.
 # This will be because some LSOAs had no crimes, and thus never even appeared in individual records.
@@ -154,9 +159,125 @@ length(unique(df_2020_sub_agg$`LSOA code`))
 missing_lsoa <- lsoa_ew_sf %>% 
   filter(lsoa_ew_sf$geo_code %nin% df_2020_sub_agg$`LSOA code`)
 
-# E01033493 is football stadium. No snap points?
+# E01033493 is a football stadium. No roads therefore no snap points.
 # E01032775 only covers ends of streets near river - may have missed the crime snap points.
-# E01004711 has some construction work, and a break in Google Street View availability. This
-#           indicates another snap point street quirk.
-# E01003179  appears to not contain actually roads which could therefore miss snap points.
+# E01004711 has some construction work, and a break in Google Street View availability (on 11 January 2012).
+#           This indicates another snap point street quirk.
+# E01003179  appears to not contain any car-worthy roads are could therefore miss snap points.
+
+# Conclusion: it would be misleading to say these had 'zero' crimes, so we will simply not include them
+# in any analysis. The polygons are removed from the LSOA data too.
+lsoa_ew_complete_sf <- lsoa_ew_sf %>% 
+  filter(geo_code %nin% missing_lsoa$geo_code)
+
+# Check number of LSOA in each dataset again. Now identical.
+length(unique(lsoa_ew_complete_sf$geo_code)) # 33076
+length(unique(df_2019_sub_agg$`LSOA code`))  # 33076
+length(unique(df_2020_sub_agg$`LSOA code`))  # 33076
+
+# Load in urban-rural classification.
+urban_df <- read_csv("data/Rural_Urban_Classification__2011__of_Lower_Layer_Super_Output_Areas_in_England_and_Wales.csv")
+
+# Check classications.
+table(urban_df$RUC11)
+table(urban_df$RUC11CD)
+
+# Create a broader urban and rural distinction.
+urban_df <- urban_df %>% 
+  mutate(urban_rural = if_else(condition = str_detect(string = RUC11, pattern = "Rural"), true = "Rural", false = "Urban"))
+
+# Check classications.
+table(urban_df$urban_rural)
+
+# Append to the crime data.
+df_2020_sub_agg <- left_join(df_2020_sub_agg, urban_df, by = c("LSOA code" = "LSOA11CD"))
+df_2019_sub_agg <- left_join(df_2019_sub_agg, urban_df, by = c("LSOA code" = "LSOA11CD"))
+
+# Check missings.
+sum(is.na(df_2020_sub_agg$urban_rural))
+sum(is.na(df_2019_sub_agg$urban_rural))
+
+# Append to spatial data.
+lsoa_ew_complete_sf <- left_join(lsoa_ew_complete_sf, urban_df, by = c("geo_code" = "LSOA11CD"))
+
+# Check (takes a while).
+# ggplot(data = lsoa_ew_complete_sf) +
+#   geom_sf(mapping = aes(fill = urban_rural), colour = "transparent")
+
+# Remove objects to retain memory if needed.
+rm(lsoa_ew_sf, lsoa_sf, df_2020_agg, df_2019_agg)
+
+# Combine 2019 and 2029 datasets for next steps.
+df_1920_df <- bind_rows(df_2019_sub_agg, df_2020_sub_agg)
+
+# Calculate E&W-wide Generalized Gini coefficient for each year.
+df_1920_total_gini <- df_1920_df %>%
+  group_by(Year, Month, `Crime type`) %>% 
+  summarise(gini_coef = gini(crime_count, generalized = TRUE, unbiased = TRUE)) %>% 
+  ungroup() %>% 
+  separate(Month, into = c("Year_temp","Month"), sep = "-") %>% 
+  select(-Year_temp) 
+
+# Initial plot.
+total_gini_gg <- ggplot(data = df_1920_total_gini) +
+  geom_line(mapping = aes(x = Month, y = gini_coef, group = `Crime type`, colour = `Crime type`)) +
+  facet_wrap(~ Year) +
+  scale_x_discrete(labels = str_extract(month.name, "^.{3}")) +
+  theme_bw()
+
+# Save.
+ggsave(filename = "visuals/total_gini.png", height = 9, width = 25, unit = "cm")
+
+
+# Do the same but group by the urban rural distinction.
+df_1920_rural_gini <- df_1920_df %>%
+  group_by(Year, Month, `Crime type`, urban_rural) %>% 
+  summarise(gini_coef = gini(crime_count, generalized = TRUE, unbiased = TRUE)) %>% 
+  ungroup() %>% 
+  separate(Month, into = c("Year_temp","Month"), sep = "-") %>% 
+  select(-Year_temp) 
+
+# Initial plot.
+rural_gini_gg <- ggplot(data = df_1920_rural_gini) +
+  geom_line(mapping = aes(x = Month, y = gini_coef, group = `Crime type`, colour = `Crime type`)) +
+  geom_vline(xintercept = 3.7, linetype = "dotted") +
+  scale_x_discrete(labels = str_extract(month.name, "^.{3}")) +
+  facet_wrap(~ urban_rural + Year) +
+  theme_bw()
+  
+# Save.
+ggsave(filename = "visuals/rural_gini.png", height = 16, width = 25, unit = "cm")
+
+# Global Moran's I for E&W.
+
+# Creating queens continuity matrix seems problematic due to memory.
+# lsoa_nb <- poly2nb(pl = lsoa_ew_complete_sf, queen = TRUE)
+
+# Save lsoa_ew_complete_sf object as shapefile for use in GeoDa.
+# st_write(obj = lsoa_ew_complete_sf, dsn = "data/lsoa_ew_complete_sf.shp")
+
+# Queens contuinity matrix created in GeoDa Version 1.14.0. Descriptive statistics screenshot
+# has been saved in the 'img' folder of this project. Warning message over 'islands' to be resolved.
+
+
+# # Create deciles for ASB as a test:
+# asb_2020_df <- df_2020_sub_agg %>% 
+#   filter(`Crime type` == "Anti-social behaviour") %>% 
+#   group_by(Month) %>% 
+#   arrange(crime_count) %>% 
+#   mutate(decile = ntile(crime_count, 10)) %>% 
+#   ungroup() %>% 
+#   arrange(decile)
+# 
+# # How many ASB counts in each LSOA, on each month?
+# nrow(filter(asb_2020_df, crime_count == 0)) # 69k zeros (19%)
+# nrow(filter(asb_2020_df, crime_count == 1)) # 62k one crime count (17%)
+#   
+# # Many zeros and low counts. Percentage change analysis at this level would not be insightful or useful.
+# # Talking about decile change would also be odd, because so many are 'draws'.
+
+
+
+
+
 
