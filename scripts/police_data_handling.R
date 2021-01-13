fb9af5d1c84a87f611b3911964554ef7be080ff3
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2021-01-13 17:48:17 +0000

    spatial prep

--- scripts/police_data_handling.R
+++ scripts/police_data_handling.R
@@ -9,7 +9,8 @@ library(stringr)
 library(ggplot2)
 library(sf)
 
-
+# Useful function.
+`%nin%` <- Negate(`%in%`)
 
 # Archive data downloaded January 2012, covering the 3-year period up to and including November 2020.
 # download.file(url = "https://data.police.uk/data/archive/2020-11.zip", destfile = "data/archive2020-11.zip")
@@ -58,8 +59,8 @@ sum(is.na(df_2019_sub$Month))
 sum(is.na(df_2020_sub$`Crime type`))
 sum(is.na(df_2019_sub$`Crime type`))
 
-sum(is.na(df_2020_sub$`LSOA code`)) # 97935 crimes have no LSOA.
-sum(is.na(df_2019_sub$`LSOA code`)) # 160541 crimes have no LSOA.
+sum(is.na(df_2020_sub$`LSOA code`)) # ~96k crimes have no LSOA.
+sum(is.na(df_2019_sub$`LSOA code`)) # ~160k crimes have no LSOA.
 
 # Drop crimes with missing LSOA (!!!).
 df_2020_nm_sub <- drop_na(df_2020_sub, `LSOA code`)
@@ -91,10 +92,6 @@ length(unique(df_2019_agg$`LSOA code`))
 # Remove previous objects to free up memory if needed.
 rm(df_2020_nm_sub, df_2019_nm_sub)
 
-# Save and load workspace as appropriate.
-# save.image(file = "data_handling.RData")
-load(file = "data_handling.RData")
-
 # To create zeros for the no-show LSOAs, first load in a complete set of LSOA codes. 
 
 # Download LSOA from UK Data Service.
@@ -104,13 +101,41 @@ load(file = "data_handling.RData")
 # Unzip.
 # unzip(zipfile = "data/ukds_infuse_lsoa.zip", exdir = "data/ukds_infuse_lsoa")
 
-# Useful function.
-`%nin%` <- Negate(`%in%`)
-
 # Load.
 lsoa_sf <- st_read("data/ukds_infuse_lsoa/infuse_lsoa_lyr_2011_clipped.shp")
 
-# Identify LSOA within Northern Ireland and Greater Manchester. We have removed those crimes reported by
+# Subset LSOAs for E&W and then remove Greater Manchester.
+lsoa_ew_sf <- lsoa_sf %>% 
+  mutate(country_cd = str_extract(geo_code, "^.{1}")) %>% 
+  filter(country_cd == "E" | country_cd == "W",
+         !str_detect(geo_label, "Manchester|Bolton|Oldham|Rochdale|Stockport|Tameside|Salford|Bury|Trafford|Wigan"))
+
+# Check validity of the remaining LSOAs. QGIS exploration indicated that there were four.
+validity_check <- st_is_valid(lsoa_ew_sf)
+
+# This confirms the four.
+table(validity_check)
+
+# Identify them
+lsoa_ew_sf <- lsoa_ew_sf %>% 
+  mutate(valid = validity_check)
+
+filter(lsoa_ew_sf, valid == "FALSE")
+
+# Try to resolve invalid geometries.
+lsoa_ew_valid_sf <- st_make_valid(lsoa_ew_sf)
+
+# Check. It worked.
+table(st_is_valid(lsoa_ew_valid_sf))
+
+# There are multiple islands in the dataset, but most are not considered a problem, since they are LSOAs
+# which share areas of the mainland or part of major islands (e.g. Isle of Wight). Of course,
+# crimes on the islands cannot be considered 'neighbours' with those on the shore, but so few crimes
+# actually occurr on these islands that it was deemed less of a problem than arbitarily removing them.
+# That said, one island has no corresponding LSOA on the mainland, and thus would have no neighbours in
+# a continuity matrix. This becomes apparent later - just making a note for now.
+
+# First, we identify LSOA within Northern Ireland and Greater Manchester. We have removed those crimes reported by
 # these forces, but for consistency we also ensure that any crimes reported by other forces falling within
 # their boundaries are removed.
 
@@ -132,11 +157,6 @@ df_2020_sub_agg <- df_2020_agg %>%
 df_2019_sub_agg <- df_2019_agg %>% 
   filter(`LSOA code` %nin% gm_lsoa, `LSOA code` %nin% ni_lsoa)
 
-# Now do the same, but for the LSOA boundaries. 
-lsoa_ew_sf <- lsoa_sf %>% 
-  mutate(country_cd = str_extract(geo_code, "^.{1}")) %>% 
-  filter(country_cd == "E" | country_cd == "W",
-         !str_detect(geo_label, "Manchester|Bolton|Oldham|Rochdale|Stockport|Tameside|Salford|Bury|Trafford|Wigan"))
 
 # Check that the crime data also doesn't include LSOAs from outside of England and Wales.
 df_2020_sub_agg <- df_2020_sub_agg %>% 
@@ -150,7 +170,7 @@ df_2019_sub_agg <- df_2019_sub_agg %>%
 table(df_2019_sub_agg$country_cd) # Confirmed.
 
 # Check LSOA in each. Confirms that police data has slightly less (N = 4) LSOA than the total data.
-length(unique(lsoa_ew_sf$geo_code))
+length(unique(lsoa_ew_valid_sf$geo_code))
 length(unique(df_2019_sub_agg$`LSOA code`))
 length(unique(df_2020_sub_agg$`LSOA code`))
 
@@ -167,13 +187,17 @@ missing_lsoa <- lsoa_ew_sf %>%
 
 # Conclusion: it would be misleading to say these had 'zero' crimes, so we will simply not include them
 # in any analysis. The polygons are removed from the LSOA data too.
-lsoa_ew_complete_sf <- lsoa_ew_sf %>% 
+lsoa_ew_valid_sf <- lsoa_ew_valid_sf %>% 
   filter(geo_code %nin% missing_lsoa$geo_code)
 
 # Check number of LSOA in each dataset again. Now identical.
-length(unique(lsoa_ew_complete_sf$geo_code)) # 33076
+length(unique(lsoa_ew_valid_sf$geo_code)) # 33076
 length(unique(df_2019_sub_agg$`LSOA code`))  # 33076
 length(unique(df_2020_sub_agg$`LSOA code`))  # 33076
+
+# Save and load workspace as appropriate.
+# save.image(file = "data_handling.RData")
+# load(file = "data_handling.RData")
 
 # Load in urban-rural classification.
 urban_df <- read_csv("data/Rural_Urban_Classification__2011__of_Lower_Layer_Super_Output_Areas_in_England_and_Wales.csv")
@@ -198,7 +222,7 @@ sum(is.na(df_2020_sub_agg$urban_rural))
 sum(is.na(df_2019_sub_agg$urban_rural))
 
 # Append to spatial data.
-lsoa_ew_complete_sf <- left_join(lsoa_ew_complete_sf, urban_df, by = c("geo_code" = "LSOA11CD"))
+lsoa_ew_valid_sf <- left_join(lsoa_ew_valid_sf, urban_df, by = c("geo_code" = "LSOA11CD"))
 
 # Check (takes a while).
 # ggplot(data = lsoa_ew_complete_sf) +
@@ -248,17 +272,35 @@ rural_gini_gg <- ggplot(data = df_1920_rural_gini) +
 # Save.
 ggsave(filename = "visuals/rural_gini.png", height = 16, width = 25, unit = "cm")
 
+# Remove objects to free memory as needed.
+rm(df_2019_sub_agg, df_2020_sub_agg)
+
+# Save and load workspace as appropriate.
+# save.image(file = "data_handling.RData") # ***Last save*** dated 13 January 2021.
+load(file = "data_handling.RData")
+
 # Global Moran's I for E&W.
 
-# Creating queens continuity matrix seems problematic due to memory.
+# Creating queens continuity matrix is problematic due to memory.
 # lsoa_nb <- poly2nb(pl = lsoa_ew_complete_sf, queen = TRUE)
 
 # Save lsoa_ew_complete_sf object as shapefile for use in GeoDa.
-# st_write(obj = lsoa_ew_complete_sf, dsn = "data/lsoa_ew_complete_sf.shp")
+# st_write(obj = lsoa_ew_valid_sf, dsn = "data/lsoa_ew_valid_sf.shp")
 
-# Queens contuinity matrix created in GeoDa Version 1.14.0. Descriptive statistics screenshot
-# has been saved in the 'img' folder of this project. Warning message over 'islands' to be resolved.
+# Rook contuinity matrix created in GeoDa Version 1.14.0. Descriptive statistics screenshot
+# has been saved in the 'img' folder of this project.
+lsoa_gal <- read.gal(file = "data/lsoa_ew_valid_sf.gal", override.id = TRUE)
+lsoa_listW <- nb2listw(lsoa_gal, style = "W", zero.policy = TRUE)
 
+# Check summary. Zero poly is TRUE because GeoDa warned about an island LSOA (which has no neighbour,
+# and also no part of the LSOA on mainland - the Isle of Scilly). This is confirmed using spdep.
+summary(lsoa_listW, zero.policy = TRUE)
+
+# We now remove the Isle of Scilly LSOA.
+lsoa_ew_valid_noisl_sf <- lsoa_ew_valid_sf %>% 
+  filter(geo_code != "E01019077")
+
+# Append the crime data to the spatial object in order to compute the Global Moran's I.
 
 # # Create deciles for ASB as a test:
 # asb_2020_df <- df_2020_sub_agg %>% 
